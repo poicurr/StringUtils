8a6745e908d8bdae24f834988e3285d0bd5237cb
poicurr <ibot07092@gmail.com>
2022-11-27 22:55:16 +0900

    add url encode/decode

--- include/StringUtils.hpp
+++ include/StringUtils.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 inline char toLower(char c) { return 'A' <= c && c <= 'Z' ? c + 'a' - 'A' : c; }
@@ -69,3 +70,40 @@ std::string trimRight(const std::string& s) {
 std::string trim(const std::string& s) { return trimLeft(trimRight(s)); }
 
 std::string repeat(char c, size_t n) { return std::string(n, c); }
+
+std::string encodeURL(char c) {
+  static const char a[] = "0123456789ABCDEF";
+  auto ret = std::string{};
+  ret += a[(0xf0 & c) >> 4];
+  ret += a[0x0f & c];
+  return ret;
+}
+
+std::string encodeURL(const std::string& s) {
+  auto ret = std::string{};
+  for (char c : s) {
+    ret += "%" + encodeURL(c);
+  }
+  return ret;
+}
+
+char decode(const std::string& s) {
+  static const std::unordered_map<char, unsigned> m = {
+      {'0', 0},  {'1', 1},  {'2', 2},  {'3', 3},  {'4', 4},  {'5', 5},
+      {'6', 6},  {'7', 7},  {'8', 8},  {'9', 9},  {'A', 10}, {'B', 11},
+      {'C', 12}, {'D', 13}, {'E', 14}, {'F', 15},
+  };
+  uint8_t c1 = (m.at(s[0]) << 4);
+  uint8_t c2 = m.at(s[1]);
+  return static_cast<char>(c1 | c2);
+}
+
+std::string decodeURL(const std::string& s) {
+  auto v = split(s, "%");
+  auto ret = std::string{};
+  for (auto s : v) {
+    if (s.empty()) continue;
+    ret += decode(s);
+  }
+  return ret;
+}
